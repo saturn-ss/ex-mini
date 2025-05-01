eacba83119d11d7a4289a511a53ffc8a44b1e956
saturn-ss <shopify.saturn@outlook.com>
2025-05-01 09:37:43 -0400

    connect admin extension

--- extensions/issue-tracker-action/src/ActionExtension.jsx
+++ extensions/issue-tracker-action/src/ActionExtension.jsx
@@ -31,14 +31,22 @@ export default reactExtension(TARGET, () => <App />);
 
 function App() {
   // connect with the extenstion's APIs
-  const { close, data } = useApi(TARGET);
+  const { close, data, intents } = useApi(TARGET);
+  const issueId = intents?.launchUrl
+    ? new URL(intents?.launchUrl)?.searchParams?.get("issueId")
+    : null;
+  const [loading, setLoading] = useState(issueId ? true : false);
   const [issue, setIssue] = useState({title: "", description: ""});
   const [allIssues, setAllIssues] = useState([]);
   const [formErrors, setFormErrors] = useState(null);
   const { title, description } = issue;
+  const isEditing = id !== undefined;
 
   useEffect(() => {
-    getIssues(data.selected[0].id).then((issues) => setAllIssues(issues || []));
+    getIssues(data.selected[0].id).then((issues) => {
+      setLoading(false);
+      setAllIssues(issues || []);
+    });
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
@@ -48,25 +56,50 @@ function App() {
     setFormErrors(errors);
 
     if (isValid) {
-      // Commit changes to the database
-      await updateIssues(data.selected[0].id, [
-        ...allIssues,
-        {
-          id: generatedId(allIssues),
-          completed: false,
+      const newIssues = [...allIssues];
+      if (isEditing) {
+        // Find the index of the issue that you're editing
+        const editingIssueIndex = newIssues.findIndex(
+          (listIssue) => listIssue.id == issue.id
+        );
+        // Overwrite that issue's title and description with the new ones
+        newIssues[editingIssueIndex] = {
           ...issue,
-        },
-      ]);
+          title,
+          description,
+        };
+      } else {
+        // Add a new issue at the end of the list
+        newIssues.push({
+          id: generatedId(allIssues),
+          title,
+          description,
+          completed: false,
+        });
+      }
+      // Commit changes to the database
+      await updateIssues(data.selected[0].id, newIssues);
       // Close the modal using the 'close' API
       close();
     }
-  }, [issue, data.selected, allIssues, close]);
+  }, [issue, allIssues, isEditing, data.selected, close, title, description]);
+
+  useEffect(() => {
+    if (issueId) {
+      // If opened from the block extension, you find the issue that's being edited
+      const editingIssue = allIssues.find(({ id }) => `${id}` === issueId);
+      if (editingIssue) {
+        // Set the issue's ID in the state
+        setIssue(editingIssue);
+      }
+    }
+  }, [issueId, allIssues])
 
   return (
     <AdminAction
-      title="Create an issue"
+      title={isEditing ? "Edit your issue" : "Create an issue"}
       primaryAction={
-        <Button onPress={onSubmit}>Create</Button>
+        <Button onPress={onSubmit}>{isEditing ? "Save" : "Create"}</Button>
       }
       secondaryAction={
         <Button onPress={close}>
--- extensions/issue-tracker-block/src/BlockExtension.jsx
+++ extensions/issue-tracker-block/src/BlockExtension.jsx
@@ -20,10 +20,10 @@ import { getIssues, updateIssues } from "./utils";
 const TARGET = "admin.product-details.block.render";
 export default reactExtension(TARGET, () => <App />);
 
-const PAGE_SIZE = 3;
+const PAGE_SIZE = 2;
 
 function App() {
-  const { data, i18n } = useApi(TARGET);
+  const { navigation, data, i18n } = useApi(TARGET);
   const [loading, setLoading] = useState(true);
   const [initialValues, setInitialValues] = useState([]);
   const [issues, setIssues] = useState([]);
@@ -150,7 +150,22 @@ function App() {
                           />
                         </Box>
                         <Box inlineSize="25%">
-                          <InlineStack inlineSize="100%" inlineAlignment="end">
+                          <InlineStack
+                            inlineSize="100%"
+                            inlineAlignment="end"
+                            blockAlignment="center"
+                            gap="base"
+                          >
+                            <Button
+                              variant="tertiary"
+                              onPress={() =>
+                                navigation?.navigate(
+                                  `extension:issue-tracker-action?issueId=${id}`
+                                )
+                              }
+                            >
+                              <Icon name="EditMinor" />
+                            </Button>
                             <Button
                               onPress={() => handleDelete(id)}
                               variant="tertiary"
@@ -165,6 +180,14 @@ function App() {
                 );
               }
             )}
+            <Divider />
+            <Box paddingBlockStart="base">
+              <Button
+                onPress={() => navigation?.navigate(`extension:issue-tracker-action`)}
+              >
+                Add issue
+              </Button>
+            </Box>
             <InlineStack
               paddingBlockStart='large'
               blockAlignment="center"
@@ -196,6 +219,11 @@ function App() {
             <Box paddingBlockEnd="large">
               <Text fontWeight="blod">No issues for this product</Text>
             </Box>
+            <Button
+              onPress={() => navigation?.navigate(`extension:issue-tracker-action`)}
+            >
+              Add your first issue
+            </Button>
           </>
         )}
       </Form>
