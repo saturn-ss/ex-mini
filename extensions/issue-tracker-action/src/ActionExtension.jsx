fe83c3ed70e291b4dc75f0298dd13e6e1e76beb6
saturn-ss <shopify.saturn@outlook.com>
2025-05-01 12:24:45 -0400

    Refactor issue handling and add recommendation API for auto-filling issue details

--- extensions/issue-tracker-action/src/ActionExtension.jsx
+++ extensions/issue-tracker-action/src/ActionExtension.jsx
@@ -1,16 +1,20 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from "react";
 import {
+  reactExtension,
   useApi,
   TextField,
   AdminAction,
   Button,
   TextArea,
-  Box,
-  reactExtension,
-} from '@shopify/ui-extensions-react/admin';
-import { getIssues, updateIssues } from './utils';
+  BlockStack,
+  Text,
+  ProgressIndicator,
+  InlineStack,
+  Banner,
+} from "@shopify/ui-extensions-react/admin";
+import { getIssues, updateIssues } from "./utils";
 
-function generatedId (allIssues) {
+function generateId (allIssues) {
   return !allIssues?.length ? 0 : allIssues[allIssues.length - 1].id + 1;
 };
 
@@ -24,35 +28,53 @@ function validateForm ({title, description}) {
   };
 };
 
-// The target used here must the target used in the extension's .toml file at ./shopify.extension.toml
-const TARGET = 'admin.product-details.action.render';
+// The target used here must match the target used in the extension's .toml file at ./shopify.ui.extension.toml
+const TARGET = "admin.product-details.action.render";
 
 export default reactExtension(TARGET, () => <App />);
 
 function App() {
-  // connect with the extenstion's APIs
   const { close, data, intents } = useApi(TARGET);
   const issueId = intents?.launchUrl
     ? new URL(intents?.launchUrl)?.searchParams?.get("issueId")
     : null;
-  const [loading, setLoading] = useState(issueId ? true : false);
-  const [issue, setIssue] = useState({title: "", description: ""});
+  const [loadingInfo, setLoadingInfo] = useState(issueId ? true : false);
+  const [loadingRecommended, setLoadingRecommended] = useState(false);
+  const [issue, setIssue] = useState({ title: "", description: "" });
   const [allIssues, setAllIssues] = useState([]);
   const [formErrors, setFormErrors] = useState(null);
-  const { title, description, id } = issue;
-  const isEditing = id !== undefined;
+  const [isEditing, setIsEditing] = useState(false);
+
 
   useEffect(() => {
     getIssues(data.selected[0].id).then((issues) => {
-      setLoading(false);
+      setLoadingInfo(false);
       setAllIssues(issues || []);
     });
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const getIssueRecommendation = useCallback(async () => {
+    // Get a recommended issue title and description from your app's backend
+    setLoadingRecommended(true);
+    // fetch is automatically authenticated and the path is resolved against your app's URL
+    const res = await fetch(
+      `api/recommendedProductIssue?productId=${data.selected[0].id}`,
+    );
+    setLoadingRecommended(false);
+
+    if (!res.ok) {
+      console.error("Network error");
+    }
+    const json = await res.json();
+    if (json?.productIssue) {
+      // If you get an recommendation, then update the title and description fields
+      setIssue(json?.productIssue);
+    }
+  }, [data.selected]);
+
   const onSubmit = useCallback(async () => {
-    const { isValid, errors} = validateForm(issue);
+    const {isValid, errors} = validateForm(issue);
     setFormErrors(errors);
 
     if (isValid) {
@@ -60,40 +82,48 @@ function App() {
       if (isEditing) {
         // Find the index of the issue that you're editing
         const editingIssueIndex = newIssues.findIndex(
-          (listIssue) => listIssue.id == issue.id
+          (listIssue) => listIssue.id == issue.id,
         );
         // Overwrite that issue's title and description with the new ones
         newIssues[editingIssueIndex] = {
           ...issue,
-          title,
-          description,
+          title: issue.title,
+          description: issue.description,
         };
       } else {
         // Add a new issue at the end of the list
         newIssues.push({
-          id: generatedId(allIssues),
-          title,
-          description,
+          id: generateId(allIssues),
+          title: issue.title,
+          description: issue.description,
           completed: false,
         });
       }
+
       // Commit changes to the database
       await updateIssues(data.selected[0].id, newIssues);
-      // Close the modal using the 'close' API
+      // Close the modal
       close();
     }
-  }, [issue, allIssues, isEditing, data.selected, close, title, description]);
+  }, [allIssues, close, data.selected, isEditing, issue]);
 
   useEffect(() => {
     if (issueId) {
-      // If opened from the block extension, you find the issue that's being edited
+      // If opened from the block extension, then find the issue that's being edited
       const editingIssue = allIssues.find(({ id }) => `${id}` === issueId);
       if (editingIssue) {
         // Set the issue's ID in the state
         setIssue(editingIssue);
+        setIsEditing(true);
       }
+    } else {
+      setIsEditing(false);
     }
-  }, [issueId, allIssues])
+  }, [issueId, allIssues]);
+
+  if (loadingInfo) {
+    return <></>;
+  }
 
   return (
     <AdminAction
@@ -101,30 +131,48 @@ function App() {
       primaryAction={
         <Button onPress={onSubmit}>{isEditing ? "Save" : "Create"}</Button>
       }
-      secondaryAction={
-        <Button onPress={close}>
-          Cancel
-        </Button>
-      }
+      secondaryAction={<Button onPress={close}>Cancel</Button>}
     >
-      <TextField
-        value={title}
-        error={formErrors?.title ? "Please enter a title" : undefined}
-        onChange={(val) => setIssue((prev) => ({...prev, title: val}))}
-        label="Title"
-        maxLength={50}
-      />
-      <Box paddingBlockStart="large">
+
+      {/*Create a banner to let the buyer auto fill the issue with the
+      recommendation from the backend*/}
+      <BlockStack gap="base">
+        <Banner>
+          <BlockStack gap="base">
+            <Text>
+              Automatically fill the issue based on past customer feedback
+            </Text>
+            <InlineStack blockAlignment="center" gap="base">
+              {/*When the button is pressed, fetch the reccomendation*/}
+              <Button
+                onPress={getIssueRecommendation}
+                disabled={loadingRecommended}
+              >
+                Generate issue
+              </Button>
+              {loadingRecommended && <ProgressIndicator size="small-100" />}
+            </InlineStack>
+          </BlockStack>
+        </Banner>
+
+        <TextField
+          value={issue.title}
+          error={formErrors?.title ? "Please enter a title" : undefined}
+          onChange={(val) => setIssue((prev) => ({ ...prev, title: val }))}
+          label="Title"
+        />
+
         <TextArea
-          value={description}
+          value={issue.description}
           error={
             formErrors?.description ? "Please enter a description" : undefined
           }
-          onChange={(val) => setIssue((prev) => ({...prev, description: val}))}
+          onChange={(val) =>
+            setIssue((prev) => ({ ...prev, description: val }))
+          }
           label="Description"
-          maxLength={300}
         />
-      </Box>
+      </BlockStack>
     </AdminAction>
-  )
+  );
 }
